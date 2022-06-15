f2aa4a39f244a23683973b26a3636b35ef5c8c72
Jose-sanmiguel <86948429+Jose-sanmiguel@users.noreply.github.com>
2022-06-15 16:47:42 +1200

    se ha hecho el dise;o del primer layout y se ha hecho la coneccion a viewbinding.

--- app/src/main/java/com/example/intents/MainActivity.kt
+++ app/src/main/java/com/example/intents/MainActivity.kt
@@ -2,15 +2,14 @@ package com.example.intents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.intents.databinding.ActivityMainBinding
 
+private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        println("Hola mundo")
-        println("Hola mundo")
-        println("Hola mundo")
-        println("Hola mundo")
-        println("Hola mundo")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
