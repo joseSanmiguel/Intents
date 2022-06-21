285643b853996d5b84efb0c068767e93e2f098b2
Jose-sanmiguel <86948429+Jose-sanmiguel@users.noreply.github.com>
2022-06-21 21:55:05 +1200

    se ha creado el segundo activity conectado con el Intent, sin embargo aun no funciona del todo debido a que falta los campos del segundo Activity

--- app/src/main/java/com/example/intents/MainActivity.kt
+++ app/src/main/java/com/example/intents/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.example.intents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.intents.databinding.ActivityMainBinding
 
+private lateinit var binding: ActivityMainBinding
+const val EXTRA_MESSAGE = "com.example.intents.MESSAGE"
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // :) comentario
+        //call function sendMessage() after click on button.
+        binding.button.setOnClickListener{sendMessage()}
+    }
+
+    private fun sendMessage(){
+        val campo = binding.etNombre
+        val message = campo.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
